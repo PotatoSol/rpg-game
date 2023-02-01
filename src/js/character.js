420d6738667dc276ec36ede8eafe25c0416cffe9
Michael Sol <sol.mike.j@gmail.com>
2023-02-01 09:09:05 -0800

    created character obj file

---
+++ src/js/character.js
@@ -0,0 +1,9 @@
+/* Character = [
+    class: [Warrior, Mage, Archer, Rogue] 
+    race: [Human, Elf, Goblin, Orc]
+    attributes: [Health, Mana]
+    stats: [Strength, Intelligence, Dexterity, Luck]
+    level: [1-100]
+    equipped: [main hand, off hand, head, chest, legs, feet]
+    inventory: [quick slots (0-9), total (99)]
+*/
