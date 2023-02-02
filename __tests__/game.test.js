cd002f11eecf90d8c2962008d692f253a1b691c4
PotatoSol <sol.mike.j@gmail.com>
2023-02-02 10:21:14 -0800

    separate tests

--- __tests__/game.test.js
+++ __tests__/game.test.js
@@ -9,10 +9,22 @@ test('Declare a new Game object', () => {
     const myPlayerChar = new PlayerCharacter(1, 'human', 'mage', [], [1, 1], [10, 11, 12, 13], 'john', []);
     const myMonster = new MonsterCharacter(1, 'orc', 'warrior', [], [1, 1], [10, 11, 12, 13], 6, ['horn']);
     newGame.setPlayer(myPlayerChar);
+    expect(newGame.player).toEqual(myPlayerChar);
+});
+test('Add a monster', () => {
+    const newGame = new Game();
+    const myPlayerChar = new PlayerCharacter(1, 'human', 'mage', [], [1, 1], [10, 11, 12, 13], 'john', []);
+    const myMonster = new MonsterCharacter(1, 'orc', 'warrior', [], [1, 1], [10, 11, 12, 13], 6, ['horn']);
+    newGame.setPlayer(myPlayerChar);
     newGame.addMonster(myMonster);
     expect(newGame.monsters).toEqual([myMonster]);
-    newGame.removeMonster(myMonster);
-    expect(newGame.monsters).toEqual([]);
-    expect(newGame.player).toEqual(myPlayerChar);
-
+});
+test('Remove a monster', () => {
+  const newGame = new Game();
+  const myPlayerChar = new PlayerCharacter(1, 'human', 'mage', [], [1, 1], [10, 11, 12, 13], 'john', []);
+  const myMonster = new MonsterCharacter(1, 'orc', 'warrior', [], [1, 1], [10, 11, 12, 13], 6, ['horn']);
+  newGame.setPlayer(myPlayerChar);
+  newGame.addMonster(myMonster);
+  newGame.removeMonster(myMonster);
+  expect(newGame.monsters).toEqual([]);
 });
